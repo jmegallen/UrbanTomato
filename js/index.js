a5862ca5cd3cb7ceea0b670cb764ee061f34a799
Jamie Gallen <jmegallen@gmail.com>
2019-11-15 17:02:54 +0100

    GMaps JS Geolocate

--- js/index.js
+++ js/index.js
@@ -1,170 +1,13 @@
+
+
+var map, infoWindow;
+
 function initMap() {
-
-  // Create a new StyledMapType object, passing it an array of styles,
-  // and the name to be displayed on the map type control.
-  var styledMapType = new google.maps.StyledMapType(
-      [
-        {
-          elementType: 'geometry',
-          stylers: [{color: '#f5f5f5'}]
-        },
-        {
-          elementType: 'labels.icon',
-          stylers: [{visibility: 'off'}]
-        },
-        {
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#616161'}]
-        },
-        {
-          elementType: 'labels.text.stroke',
-          stylers: [{color: '#f5f5f5'}]
-        },
-        {
-          featureType: 'administrative.land_parcel',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#bdbdbd'}]
-        },
-        {
-          featureType: 'poi',
-          elementType: 'geometry',
-          stylers: [{color: '#eeeeee'}]
-        },
-        {
-          featureType: 'poi',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#757575'}]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'geometry',
-          stylers: [{color: '#e5e5e5'}]
-        },
-        {
-          featureType: 'poi.park',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#9e9e9e'}]
-        },
-        {
-          featureType: 'road',
-          elementType: 'geometry',
-          stylers: [{color: '#ffffff'}]
-        },
-        {
-          featureType: 'road.arterial',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#757575'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'geometry',
-          stylers: [{color: '#dadada'}]
-        },
-        {
-          featureType: 'road.highway',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#616161'}]
-        },
-        {
-          featureType: 'road.local',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#9e9e9e'}]
-        },
-        {
-          featureType: 'transit.line',
-          elementType: 'geometry',
-          stylers: [{color: '#e5e5e5'}]
-        },
-        {
-          featureType: 'transit.station',
-          elementType: 'geometry',
-          stylers: [{color: '#eeeeee'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'geometry',
-          stylers: [{color: '#c9c9c9'}]
-        },
-        {
-          featureType: 'water',
-          elementType: 'labels.text.fill',
-          stylers: [{color: '#9e9e9e'}]
-        }
-      ],
-      {name: 'Greyscale'});
-
-  // Create a map object, and include the MapTypeId to add
-  // to the map type control.
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 17,
-    center: {lat: 52.4621432, lng: 13.4195465},
-    mapTypeControlOptions: {
-      mapTypeIds: ['roadmap',
-              'styled_map']
-    }
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 1
   });
-
-  //Associate the styled map with the MapTypeId and set it to display.
-  map.mapTypes.set('styled_map', styledMapType);
-  map.setMapTypeId('styled_map');
-
-  // This sample uses the Place Autocomplete widget to allow the user to search
-  // for and select a place. The sample then displays an info window containing
-  // the place ID and other information about the place that the user has
-  // selected.
-
-  // This example requires the Places library. Include the libraries=places
-  // parameter when you first load the API. For example:
-  // <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
-
-  var input = document.getElementById('pac-input');
-
-  var autocomplete = new google.maps.places.Autocomplete(input);
-  autocomplete.bindTo('bounds', map);
-
-  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-  var infowindow = new google.maps.InfoWindow();
-  var infowindowContent = document.getElementById('infowindow-content');
-  infowindow.setContent(infowindowContent);
-  var image = 'https://openclipart.org/image/50px/svg_to_png/225413/mini-tomato.png';
-  var marker = new google.maps.Marker({
-    map: map,
-    draggable:true,
-    icon: image
-  });
-  marker.addListener('click', function() {
-    infowindow.open(map, marker);
-  });
-
-  autocomplete.addListener('place_changed', function() {
-    infowindow.close();
-    var place = autocomplete.getPlace();
-    if (!place.geometry) {
-      return;
-    }
-
-    if (place.geometry.viewport) {
-      map.fitBounds(place.geometry.viewport);
-    } else {
-      map.setCenter(place.geometry.location);
-      map.setZoom(17);
-    }
-
-    // Set the position of the marker using the place ID and location.
-    marker.setPlace({
-      placeId: place.place_id,
-      location: place.geometry.location
-    });
-    marker.setVisible(true);
-
-    infowindowContent.children['place-name'].textContent = place.name;
-    infowindowContent.children['place-id'].textContent = place.place_id;
-    infowindowContent.children['place-address'].textContent =
-        place.formatted_address;
-    infowindow.open(map, marker);
-  });
-
+  infoWindow = new google.maps.InfoWindow;
 
   // Try HTML5 geolocation.
   if (navigator.geolocation) {
@@ -178,6 +21,7 @@ function initMap() {
       infoWindow.setContent('Location found.');
       infoWindow.open(map);
       map.setCenter(pos);
+      map.setZoom(15);
     }, function() {
       handleLocationError(true, infoWindow, map.getCenter());
     });
